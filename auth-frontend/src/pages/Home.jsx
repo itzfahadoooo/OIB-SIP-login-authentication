6c8c69d94420729333d6edf73b1eb8d0d7280a95
itzfahadoooo <foyindolapo@gmail.com>
2025-03-09 22:32:11 +0100

    feat/Creation of frontend endpoints

--- auth-frontend/src/pages/Home.jsx
+++ auth-frontend/src/pages/Home.jsx
@@ -5,7 +5,10 @@ function Home() {
   return (
     <div className="home-container">
       <h1>Welcome to Auth System</h1>
+
+
       <div className="auth-links">
+        
         <Link to="/register">Register</Link> | <Link to="/login">Login</Link>
       </div>
     </div>
