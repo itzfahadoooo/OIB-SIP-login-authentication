eb490b0efb9eb12782cb49e043a358904087fbea
itzfahadoooo <foyindolapo@gmail.com>
2025-03-11 21:37:19 +0100

    feat/Creation of frontend endpoints

--- auth-frontend/src/components/dashboard.jsx
+++ auth-frontend/src/components/dashboard.jsx
@@ -1,28 +1,36 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
-import "./styles/Dashboard.css"; 
+import "./styles/Dashboard.css";
 
 function Dashboard() {
   const [message, setMessage] = useState("");
-  
+
   const navigate = useNavigate();
 
   useEffect(() => {
-    axios.get("http://localhost:3000/dashboard", { withCredentials: true })
-      .then(response => setMessage(response.data.message))
+    axios
+      .get("http://localhost:3000/dashboard", { withCredentials: true })
+
+      .then((response) => setMessage(response.data.message))
       .catch(() => navigate("/login"));
   }, []);
 
   const handleLogout = async () => {
-    await axios.post("http://localhost:3000/logout", {}, { withCredentials: true });
+    await axios.post(
+      "http://localhost:3000/logout",
+      {},
+      { withCredentials: true }
+    );
     navigate("/login");
   };
 
   return (
     <div className="dashboard-container">
       <h2>{message}</h2>
-      <button className="logout-button" onClick={handleLogout}>Logout</button>
+      <button className="logout-button" onClick={handleLogout}>
+        Logout
+      </button>
     </div>
   );
 }
