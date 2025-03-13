aee5536cd1ce60838bd0a8c1b4c2d7bea86c6d41
itzfahadoooo <foyindolapo@gmail.com>
2025-03-13 12:53:02 +0100

    Initial Commit

--- auth-frontend/src/components/dashboard.jsx
+++ auth-frontend/src/components/dashboard.jsx
@@ -13,6 +13,7 @@ function Dashboard() {
       .get("http://localhost:3000/dashboard", { withCredentials: true })
 
       .then((response) => setMessage(response.data.message))
+
       .catch(() => navigate("/login"));
   }, []);
 
