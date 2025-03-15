090f74eb662aa73caf41a237968ca9f5fd4058b6
itzfahadoooo <foyindolapo@gmail.com>
2025-03-15 21:07:49 +0100

    Fix bcrypt deployment issue

--- auth-backend/server.js
+++ auth-backend/server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const fs = require("fs");
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 const session = require("express-session");
 const cors = require("cors");
 
